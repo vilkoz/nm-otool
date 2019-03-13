a8ad64d7cb8706d76fc37435732e111ee3c8c070
Vitalii Rybalko <vilko.a@gmail.com>
2019-03-14 00:47:37 +0200

    nm: upd test to support fat_binary
    Signed-off-by: Vitalii Rybalko <vilko.a@gmail.com>

--- test.fish
+++ test.fish
@@ -4,16 +4,10 @@ make
 set bins $argv
 
 for name in $bins
-	#if file $name | head -1 | grep -qo "Mach-O 64-bit executable x86_64"
-		if file $name | head -1 | grep -qo "Mach-O universal binary with"
-			echo "FAT BINARY NOT SUPPORTED"
-		else
-			echo $name
-			nm $name > a;
-			./nm $name > b;
-			diff a b;
-			rm a b
-			echo "end $name coparison"
-		end
-	#end
+	echo $name
+	nm $name > a;
+	./nm $name > b;
+	diff a b;
+	rm a b
+	echo "end $name coparison"
 end
