968825943c71f442a226bd2d198375578f8becb3
vilkoz <vilko.a@gmail.com>
2019-03-17 23:36:26 +0200

    nm: test.bash available for fat_binaries

--- test.bash
+++ test.bash
@@ -5,14 +5,10 @@ bins=$@
 echo $bins
 
 for name in $bins; do
-	if file $name | head -1 | grep -qo "Mach-O universal binary with"; then
-		echo "FAT BINARY NOT SUPPORTED";
-	else
-		echo $name;
-		nm $name > a;
-		./nm $name > b;
-		diff a b;
-		rm a b;
-		echo "end $name coparison";
-	fi
+	echo $name;
+	nm $name > a;
+	./nm $name > b;
+	diff a b;
+	rm a b;
+	echo "end $name coparison";
 done
